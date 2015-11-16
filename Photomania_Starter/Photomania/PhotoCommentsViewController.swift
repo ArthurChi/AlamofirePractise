49951df213921548946fbce2c52988bcbfbd9057
cjfire <cjfire@sina.cn>
2015-11-16 12:26:02 +0800

    为cell添加模型

--- Photomania_Starter/Photomania/PhotoCommentsViewController.swift
+++ Photomania_Starter/Photomania/PhotoCommentsViewController.swift
@@ -7,51 +7,76 @@
 //
 
 import UIKit
+import Alamofire
 
 class PhotoCommentsViewController: UITableViewController {
-  var photoID: Int = 0
-  var comments: [Comment]?
-  
-  // MARK: Life-Cycle
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
+    var photoID: Int = 0
+    var comments: [Comment]?
     
-    tableView.rowHeight = UITableViewAutomaticDimension
-    tableView.estimatedRowHeight = 50.0
+    // MARK: Life-Cycle
     
-    title = "Comments"
-    navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Done", style: .Done, target: self, action: "dismiss")
-  }
-  
-  func dismiss() {
-    dismissViewControllerAnimated(true, completion: nil)
-  }
-  
-  // MARK: - TableView
-  
-  override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return comments?.count ?? 0
-  }
-  
-  override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCellWithIdentifier("CommentCell", forIndexPath: indexPath) as! PhotoCommentTableViewCell
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 50.0
+        
+        title = "Comments"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Done", style: .Done, target: self, action: "dismiss")
+        
+        Alamofire.request(Five100px.Router.Comments(photoID, 1)).validate().responseCollection { (response :Response<[Comment],NSError>) -> Void in
+            
+            if response.result.isSuccess {
+                self.comments = response.result.value
+                self.tableView.reloadData()
+            }
+        }
+    }
     
-    return cell
-  }
+    func dismiss() {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    // MARK: - TableView
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return comments?.count ?? 0
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("CommentCell", forIndexPath: indexPath) as! PhotoCommentTableViewCell
+        
+        cell.commentItem = comments![indexPath.row]
+        return cell
+    }
 }
 
 class PhotoCommentTableViewCell: UITableViewCell {
-  @IBOutlet weak var userImageView: UIImageView!
-  @IBOutlet weak var commentLabel: UILabel!
-  @IBOutlet weak var userFullnameLabel: UILabel!
-  
-  override func awakeFromNib() {
-    super.awakeFromNib()
+    @IBOutlet weak var userImageView: UIImageView!
+    @IBOutlet weak var commentLabel: UILabel!
+    @IBOutlet weak var userFullnameLabel: UILabel!
     
-    userImageView.layer.cornerRadius = 5.0
-    userImageView.layer.masksToBounds = true
+    var commentItem: Comment? {
+        didSet {
+            
+            Alamofire.request(.GET, commentItem!.userPictureURL).validate().responseImage { (response) -> Void in
+                
+                if response.result.isSuccess {
+                    self.userImageView.image = response.result.value!
+                }
+            }
+            
+            userFullnameLabel.text = commentItem!.userFullname
+            commentLabel.text = commentItem!.commentBody
+        }
+    }
     
-    commentLabel.numberOfLines = 0
-  }
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        userImageView.layer.cornerRadius = 5.0
+        userImageView.layer.masksToBounds = true
+        
+        commentLabel.numberOfLines = 0
+    }
 }
